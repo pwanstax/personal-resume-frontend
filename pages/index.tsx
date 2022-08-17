974811cb339198669072e4862daa1a7cf006273d
Pawan Kanjeam <tutor_pawan@hotmail.com>
2022-08-18 00:00:28 +0700

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,14 @@ import "../node_modules/font-awesome/css/font-awesome.min.css";
 import AboutPage from "../components/pages/aboutPage/AboutPage";
 import HomePage from "../components/pages/homePage/HomePage";
 import WorkPage from "../components/pages/workPage/WorkPage";
+import Head from "next/head";
 
 const MainPage: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Pawan Kanjeam</title>
+      </Head>
       <HomePage />
       <div id="work" className="content-divider">
         <div></div>
