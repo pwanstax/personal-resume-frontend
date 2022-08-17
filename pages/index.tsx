0a11225db3777a4f49c0b2b271444a05360820c1
Pawan Kanjeam <tutor_pawan@hotmail.com>
2022-08-17 23:34:06 +0700

    finalize feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type {NextPage} from "next";
 import Image from "next/image";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import AboutPage from "../components/pages/aboutPage/AboutPage";
 import HomePage from "../components/pages/homePage/HomePage";
 import WorkPage from "../components/pages/workPage/WorkPage";
