013f8849c6cf721880cb88a19403240a00f1069f
Pawan Kanjeam <tutor_pawan@hotmail.com>
2022-09-05 15:51:13 +0700

    feat: add change mode icon with animation

--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -1,4 +1,28 @@
+import {motion, Variants, useAnimation} from "framer-motion";
+import {useState} from "react";
+
 const MainNavigation = () => {
+  const [isLightMode, setIsLightMode] = useState(true);
+  const controlMode = useAnimation();
+
+  const handleMode = () => {
+    controlMode.start("changemode");
+    setTimeout(function () {
+      setIsLightMode(!isLightMode);
+    }, 500);
+  };
+
+  const modeVariants: Variants = {
+    changemode: {
+      opacity: [1, 0.1, 1],
+      x: [-25, 25, -20, 20, -15, 15, -10, 10, -5, 5, 0],
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 1,
+      },
+    },
+  };
   return (
     <div className="navigation-container">
       <a href="#">Pawan Kanjeam</a>
@@ -7,6 +31,15 @@ const MainNavigation = () => {
         <a href="#aboutMe">About</a>
         <a href="#aboutMe">Contact</a>
       </div>
+      <motion.div
+        className="switch-theme"
+        whileHover={{scale: 1.025, cursor: "pointer"}}
+        onClick={handleMode}
+        animate={controlMode}
+        variants={modeVariants}
+      >
+        {isLightMode ? <p>🌕</p> : <p>🌑</p>}
+      </motion.div>
     </div>
   );
 };
