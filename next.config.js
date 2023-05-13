4a4fc07d5d50d4232211efeeef7f4ba5d1bfd70a
PawanK <tutor_pawan@hotmail.com>
2023-05-14 03:48:47 +0700

    v2.3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["*"],
+    domains: [
+      "images.unsplash.com",
+      "officesnapshots.com",
+      "cdn10.agoda.net",
+      "fastwork.co",
+      "miro.medium.com",
+      "user-images.githubusercontent.com",
+      "camo.githubusercontent.com",
+      "i.ytimg.com",
+      "is3-ssl.mzstatic.com",
+      "user-images.githubusercontent.com",
+      "i.pinimg.com",
+    ],
   },
 
   async headers() {
@@ -13,7 +25,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; font-src 'self' https://fonts.gstatic.com",
+              "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://images.unsplash.com https://officesnapshots.com https://cdn10.agoda.net https://fastwork.co https://miro.medium.com https://user-images.githubusercontent.com https://camo.githubusercontent.com https://i.ytimg.com https://is3-ssl.mzstatic.com https://i.pinimg.com",
           },
         ],
       },
