dcec853fe97e5c6b42875097971daadb46e6717b
PawanK <tutor_pawan@hotmail.com>
2023-05-14 03:53:08 +0700

    v2.5

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
       "is3-ssl.mzstatic.com",
       "user-images.githubusercontent.com",
       "i.pinimg.com",
+      "fonts.googleapis.com", // Added Google Fonts domain
     ],
   },
 
@@ -25,7 +26,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://images.unsplash.com https://officesnapshots.com https://cdn10.agoda.net https://fastwork.co https://miro.medium.com https://user-images.githubusercontent.com https://camo.githubusercontent.com https://i.ytimg.com https://is3-ssl.mzstatic.com https://i.pinimg.com",
+              "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https://fonts.googleapis.com  https://images.unsplash.com https://officesnapshots.com https://cdn10.agoda.net https://fastwork.co https://miro.medium.com https://user-images.githubusercontent.com https://camo.githubusercontent.com https://i.ytimg.com https://is3-ssl.mzstatic.com https://i.pinimg.com",
           },
         ],
       },
