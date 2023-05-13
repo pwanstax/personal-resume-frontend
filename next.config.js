9f3b515460e4e2ca307e7aa9fc84378ff35b9fc4
PawanK <tutor_pawan@hotmail.com>
2023-05-14 03:46:43 +0700

    v2.2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ["*"],
   },
+
   async headers() {
     return [
       {
